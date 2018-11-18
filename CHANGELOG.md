cf969a371fa417bafb698b6c308c5f12265cfb26
Mihai Petracovici <petracvv@users.noreply.github.com>
2018-11-18 14:21:47 -0600

    Initial v1.0.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,15 @@
-# logstash_lwrp CHANGELOG
+# logstash_lwrp Cookbook CHANGELOG
 
 This file is used to list changes made in each version of the logstash_lwrp cookbook.
 
-# 0.1.0
+## 1.0.0 (2018-11-18)
 
 Initial release.
 
-- change 0
-- change 1
-
+- Added logstash_install resource
+- Added logstash_config resource
+- Added logstash_pipeline resource
+- Added logstash_service resource
+- Added Initial README
+- Added LICENSE
+- Added Integration tests
--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,14 @@ maintainer 'Mihai Petracovici'
 maintainer_email 'petracvv@users.noreply.github.com'
 license 'Apache-2.0'
 description 'Resource-driven Logstash cookbook'
-long_description 'Installs/Configures logstash_lwrp'
-version '0.1.0'
-chef_version '>= 13.0'
-
-%w( ubuntu debian centos opensuse ).each do |os|
-  supports os
-end
+version '1.0.0'
 
 issues_url 'https://github.com/petracvv/logstash_lwrp/issues'
-
 source_url 'https://github.com/petracvv/logstash_lwrp'
+
+chef_version '>= 13.0'
+
+supports 'ubuntu', '>= 16.04'
+supports 'debian', '>= 8'
+supports 'centos', '>= 6'
+
