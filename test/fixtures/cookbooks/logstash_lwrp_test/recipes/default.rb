93c46b62ef6f06e9ee0e3fb5cca5ac366a060239
Mihai Petracovici <petracvv@users.noreply.github.com>
2018-11-18 13:35:22 -0600

    Fix test recipe. Add integration tests

--- test/fixtures/cookbooks/logstash_lwrp_test/recipes/default.rb
+++ test/fixtures/cookbooks/logstash_lwrp_test/recipes/default.rb
@@ -4,8 +4,19 @@ es_conf.allocated_memory '128m'
 es_svc = resources('elasticsearch_service[elasticsearch]')
 es_svc.service_actions [:enable, :start]
 
+# Debian 8 only has java-8 in backports
+bash 'Enable backports for java-8' do
+  code <<-EOH
+  echo -e "deb http://ftp.us.debian.org/debian/ jessie-backports main" >> /etc/apt/sources.list
+  echo -e "Package: openjdk* openjre* *java*\nPin: release a=jessie-backports\nPin-Priority: 900\n" >> /etc/apt/preferences
+  EOH
+  only_if { node['platform_family'] == 'debian' && node['platform_version'] =~ /^8/}
+  not_if 'grep jessie-backports /etc/apt/sources.list'
+end.run_action(:run)
+
 logstash_install 'kitchen' do
-  action :install
+  version '6.5.0'
+  action :upgrade
 end
 
 logstash_config 'kitchen' do
@@ -27,6 +38,16 @@ logstash_pipeline 'testing' do
   instance 'kitchen'
   pipeline_workers 1
   config_templates %w( logstash-sample.conf.erb )
+  action :create
+end
+
+logstash_pipeline 'string' do
+  instance 'kitchen'
+  pipeline_workers 1
+  config_string "input { generator {} } filter { sleep { time => 1 } } output { stdout { codec => dots } }"
+  pipeline_settings(
+    'queue.type' => 'memory' 
+  )
   action :create
 end
 
